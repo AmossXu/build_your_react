a7074f4c91ed50498b4558fa3ae3dbf3b635203f
Amos <mots.xu@gmail.com>
2022-02-27 15:33:46 +0800

    feat: compare old fiber to element

--- src/Chaos/index.js
+++ src/Chaos/index.js
@@ -47,14 +47,65 @@ function render(element, container) {
         },
         alternate: currentRoot
     }
+    deletions = []
     nextUnitOfWork = wipRoot
 }
 
 let wipRoot = null
 let currentRoot = null
 let nextUnitOfWork = null
+let deletions = null
 
+// special kind of prop lick event listeners
+const isEvent = key => key.startsWith('on')
+const isProperty = key => key !== "children" && isEvent(key)
+const isNew = (prev, next) => key =>
+    prev[key] !== next[key]
+const isGone = (next) => key => !(key in next)
+function uodateDom(fiber, prevProps, nextProps) {
+    // remote old or changed event listeners
+    Object.keys(prevProps)
+        .filter(isEvent)
+        .filter(key => !(key in nextProps) || isNew(prevProps, nextProps)(key))
+        .forEach(name => {
+            const eventType = name
+                .toLowerCase()
+                .substring(2)
+
+            dom.removeEventListener(eventType, prevProps[name])
+        })
+
+    // Remove old properties
+    Object.keys(prevProps)
+        .filter(isProperty)
+        .filter(isGone(nextProps))
+        .forEach(name => {
+            dom[name] = ""
+        })
+
+    // Set new properties
+    Object.keys(nextProps)
+        .filter(isProperty)
+        .filter(isNew(prevProps, nextProps))
+        .forEach(name => {
+            dom[name] = nextProps[name]
+        })
+
+    // add event listeners
+    Object.keys(nextProps)
+        .filter(isEvent)
+        .filter(key => isNew(prevProps, nextProps)(key))
+        .forEach(name => {
+            const eventType = name
+                .toLowerCase()
+                .substring(2)
+
+            dom.addEventListener(eventType, nextProps[name])
+        })
+
+}
 function commitRoot() {
+    deletions.forEach(commitWork)
     commitWork(wipRoot.child)
     currentRoot = wipRoot
     wipRoot = null
@@ -66,7 +117,24 @@ function commitWork(fiber) {
     }
     const domParent = fiber.parent.dom
 
-    domParent.appendChild(fiber.dom)
+    if (
+        fiber.effectTag === 'PLACEMENT' &&
+        fiber.dom !== null
+    ) {
+        domParent.appendChild(fiber.dom)
+    } else if (
+        fiber.effectTag === 'UPDATE' &&
+        fiber.dom !== null
+    ) {
+        uodateDom(
+            fiber.dom,
+            fiber.alternate.props,
+            fiber.props
+        )
+    } else if (fiber.effectTag === 'DELETION') {
+        domParent.removeChild(fiber.dom)
+    }
+
     commitWork(domParent.child)
     commitWork(domParent.sibling)
 }
@@ -103,28 +171,9 @@ function performUnitOfWork(fiber) {
     // create new fiber
     const elements = fiber.props.children
 
-    let index = 0
-    let prevSibling = null
+    reconcileChildren(fiber, elements)
 
-    while (index < elements.length) {
-        const element = elements[index]
 
-        const newFiber = {
-            type: element.type,
-            props: element.props,
-            parent: fiber,
-            dom: null
-        }
-
-        if (index === 0) {
-            fiber.child = newFiber
-        } else {
-            prevSibling.sibling = newFiber
-        }
-
-        prevSibling = newFiber
-        index++
-    }
 
     // return next unit of work
     if (fiber.child) {
@@ -139,6 +188,73 @@ function performUnitOfWork(fiber) {
     }
 }
 
+/**
+ * The element is the thing we want to render to the DOM and the oldFiber is what we rendered the last time.
+ * @param {*} wipFiber 
+ * @param {*} elements 
+ */
+function reconcileChildren(wipFiber, elements) {
+    let index = 0
+    let oldFiber = wipFiber.alternate && wipFiber.alternate.child
+    let prevSibling = null
+
+    while (index < elements.length || oldFiber != null) {
+        const element = elements[index]
+        let newFiber = null
+
+        // const newFiber = {
+        //     type: element.type,
+        //     props: element.props,
+        //     parent: fiber,
+        //     dom: null
+        // }
+
+        // compare old fiber to element
+        const sameType =
+            oldFiber &&
+            element &&
+            element.type == oldFiber.type
+
+        if (sameType) {
+            // update the node
+            newFiber = {
+                type: oldFiber.type,
+                props: element.props,
+                dom: oldFiber.dom,
+                parent: wipFiber,
+                alternate: oldFiber,
+                effectTag: 'UPDATE'
+            }
+        }
+
+        if (element && !sameType) {
+            // create new fiber
+            newFiber = {
+                type: element.type,
+                props: element.props,
+                dom: null,
+                parent: wipFiber,
+                alternate: null,
+                effectTag: 'PLACEMENT'
+            }
+        }
+
+        if (oldFiber && !sameType) {
+            // delete oldFiber
+            oldFiber.effectTag = 'DELETETION'
+            deletions.push(oldFiber)
+        }
+
+        if (index === 0) {
+            wipFiber.child = newFiber
+        } else {
+            prevSibling.sibling = newFiber
+        }
+
+        prevSibling = newFiber
+        index++
+    }
+}
 const Chaos = {
     createElement,
     render,
